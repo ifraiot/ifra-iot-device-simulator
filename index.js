a4c31759bbad4197614b2690c748737302a212a2
SHINJI <czzsevensc@gmail.com>
2020-12-11 11:18:19 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+ var mqtt = require('mqtt');
+
+const MQTT_SERVER = "<server>";         // example: staging.mqtt.ifra.io
+const MQTT_PORT = "<port>";             // Default 1883
+const MQTT_USER = "<username>";         // example: dc5ec082-084d-4cac-84e0-684b08fe4b4a
+const MQTT_PASSWORD = "<password>";     // example: 0e3ccc6d-edb1-4e32-a049-3ee38f1888de
+const TOPIC = "<topic>"                 // example: organization/a369f4b8-77bc-4b88-8e38-24cae2a33340/messages
+const MEASUREMENT_NAME = "<measurement name>"  // example: measurement
+const UNIT = "<unit>"                   // example: %
+// Connect MQTT
+var client = mqtt.connect({
+    host: MQTT_SERVER,
+    port: MQTT_PORT,
+    username: MQTT_USER,
+    password: MQTT_PASSWORD
+});
+
+client.on('connect', function () {
+    // Subscribe any topic
+    console.log("MQTT Connect");
+    client.subscribe(TOPIC, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+// Receive Message and print on terminal
+client.on('message', function (topic, message) {
+ // message is Buffer
+    console.log(message.toString());
+});
+
+// Random number 0-10 send every 5 seconds
+setInterval(() => {
+    const RANDOM_NUMBER =  Math.floor(Math.random() * 11)
+    client.publish(TOPIC, '[{ "n": "'+MEASUREMENT_NAME+'", "v": '+RANDOM_NUMBER+' , "u":"'+UNIT+'"}]');
+}, 5000);
+
+
---
+++ readme.md
@@ -0,0 +1,25 @@
+# IFRA MQTT NODEJS
+
+IFRA MQTT Nodejs test is a library for simulate data send  to ifra platform  http://iot.ifra.io.
+
+## Installation
+
+Use the package manager [MqttJS](https://www.npmjs.com/package/mqtt) to install.
+
+```bash
+npm install mqtt --save
+```
+
+## Usage
+
+```javascript
+ node index.js
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
